c720c589f7fffa9cda3d94b39df88211ee8a8fb0
Julian Bruyat <bruyat.ju@live.fr>
2021-02-26 10:58:48 +0100

    Better performances thanks to WT!

--- rdfplsinterpreter.js
+++ rdfplsinterpreter.js
@@ -1,8 +1,16 @@
 const N3 = require("n3");
+const WT = require("@bruju/wasm-tree");
 const namespace = require('@rdfjs/namespace');
 
 const rdf = namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#", N3.DataFactory);
 const pls = namespace("http://bruy.at/rdf/pls#", N3.DataFactory);
+
+function giveFirst(dataset) {
+    for (const quad of dataset) {
+        return quad;
+    }
+    return null;
+}
 
 function unlist(store, head) {
     const elements = [];
@@ -29,38 +37,38 @@ function readInstruction(store, triple) {
     }
 }
 
-function travelPath(store, start, path) {
+function travelPath(dataset, start, path) {
     for (let i = 0 ; i != path.length ; ++i) {
-        start = store.getQuads(start, path[i])[0].object;
+        start = giveFirst(dataset.match(start, path[i])).object;
     }
 
     return start;
 }
 
-function readInstructions(store, instructions) {
-    let instructions_ = unlist(store, instructions);
+function readInstructions(dataset, instructions) {
+    let instructions_ = unlist(dataset, instructions);
 
     for (const instruction of instructions_) {
-        readInstruction(store, instruction);
+        readInstruction(dataset, instruction);
     }
 }
 
-function readGraph(store) {
-    const mainFunction = store.getQuads(null, rdf.type, pls.main_function);
+function readGraph(dataset) {
+    const mainFunction = dataset.match(null, rdf.type, pls.main_function);
 
-    if (mainFunction.length !== 1) {
+    if (mainFunction.size !== 1) {
         console.error("No unique main function found");
         return 1;
     }
 
-    const mainInstructions = store.getQuads(mainFunction[0].subject, pls.instructions);
+    const mainInstructions = dataset.match(giveFirst(mainFunction).subject, pls.instructions);
 
-    if (mainInstructions.length !== 1) {
+    if (mainInstructions.size !== 1) {
         console.error("No unique instructions found");
         return 2;
     }
 
-    readInstructions(store, mainInstructions[0].object);
+    readInstructions(dataset, giveFirst(mainInstructions).object);
 }
 
 
@@ -75,7 +83,8 @@ function readFile(filepath) {
 
 const filename = process.argv[2];
 
-const graph = new N3.Store(readFile(filename));
+const graph = new WT.Dataset();
+graph.addAll(readFile(filename));
 readGraph(graph);
 
 
